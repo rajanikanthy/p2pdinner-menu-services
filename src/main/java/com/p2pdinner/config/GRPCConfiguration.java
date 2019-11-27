e5f830b57e49772e0230dc20865260a183d1197c
Kantha Reddy Yammanuru Rajani <rajaniy@Kanthas-MacBook-Pro.local>
2019-11-27 14:41:00 -0800

    Move grpc configuration to config repo

--- src/main/java/com/p2pdinner/config/GRPCConfiguration.java
+++ src/main/java/com/p2pdinner/config/GRPCConfiguration.java
@@ -3,6 +3,7 @@ package com.p2pdinner.config;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class GRPCConfiguration {
 
+    @Value("${app.dinner.listing.grpc.url}")
+    private String grpcServerUrl;
+
+    @Value("${app.dinner.listing.grpc.port}")
+    private Integer port;
+
     @Bean
     public ManagedChannel channel() {
-        final ManagedChannel channel = ManagedChannelBuilder.forAddress("listing-service", 6565)
+        final ManagedChannel channel = ManagedChannelBuilder.forAddress(grpcServerUrl, port)
                 .usePlaintext()
                 .build();
         log.info("Channel configured with parameters {}", channel);
